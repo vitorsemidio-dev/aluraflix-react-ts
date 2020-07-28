292b6c3e96211998767492711f05810c2a397395
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-27 21:28:59 -0300

    função getYouTubeId com useCallback

--- src/components/Carousel/components/VideoCard/index.tsx
+++ src/components/Carousel/components/VideoCard/index.tsx
@@ -1,13 +1,6 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
 import { VideoCardContainer } from './styles';
-
-function getYouTubeId(youtubeURL) {
-  return youtubeURL.replace(
-    /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/,
-    '$7',
-  );
-}
 
 interface VideoCardProps {
   videoTitle: string;
@@ -20,6 +13,16 @@ const VideoCard: React.FC<VideoCardProps> = ({
   videoURL,
   categoryColor,
 }) => {
+  const getYouTubeId = useCallback(
+    (youtubeURL: string) => {
+      return youtubeURL.replace(
+        /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/,
+        '$7',
+      );
+    },
+    [videoURL],
+  );
+
   const image = `https://img.youtube.com/vi/${getYouTubeId(
     videoURL,
   )}/hqdefault.jpg`;
