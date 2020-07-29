cd4f281e6a1a033e5d5881a92c10b3d320010d72
Emidio <52754546+vitorsemidio-dev@users.noreply.github.com>
2020-07-29 20:22:20 -0300

    Cadastro categoria em array na memória (#12)
    * Criação componente cadastro categoria e configuração rota
    
    * Formulário Cadastro html
    
    * Listagem categoria
    
    * Campos descrição e cor adicionados
    
    * onChange generico
    
    * ajustes onChange
    
    * Refatoração

--- src/pages/cadastro/Categoria/index.tsx
+++ src/pages/cadastro/Categoria/index.tsx
@@ -1,16 +1,102 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import PageDefault from '../../../components/PageDefault';
 
+interface Category {
+  name: string;
+  color: string;
+  description: string;
+}
+
 const Categoria: React.FC = () => {
+  const [category, setCategory] = useState<Category>({
+    name: '',
+    color: '#000000',
+    description: '',
+  });
+
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  function handleSubmit(eventSubmit: React.FormEvent<HTMLFormElement>) {
+    eventSubmit.preventDefault();
+    setCategories([...categories, category]);
+    setCategory({
+      name: '',
+      color: '#000000',
+      description: '',
+    });
+  }
+
+  function handleChange(
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) {
+    const key = event.target.getAttribute('name') || '';
+    const { value } = event.target;
+    setCategory({
+      ...category,
+      [key]: value,
+    });
+  }
+
   return (
     <PageDefault>
-      <div>
-        <h1>Cadastro Categoria</h1>
+      <h1>Cadastro de Categoria</h1>
 
-        <Link to="/">Ir para home</Link>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="categoria">
+            Nome da Categoria:
+            <input
+              id="categoria"
+              name="name"
+              type="text"
+              value={category.name}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+
+        <div>
+          <label htmlFor="description">
+            Descrição:
+            <textarea
+              id="description"
+              name="description"
+              value={category.description}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+
+        <div>
+          <label htmlFor="color">
+            Cor:
+            <input
+              type="color"
+              id="color"
+              name="color"
+              value={category.color}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+
+        <button type="submit">Cadastrar</button>
+      </form>
+
+      <ul>
+        {categories.map((categoryItem, index) => (
+          <li key={String(index)}>
+            <h3 style={{ color: categoryItem.color }}>
+              {`${index + 1} - ${categoryItem.name}`}
+            </h3>
+            <p>Descrição: {categoryItem.description}</p>
+          </li>
+        ))}
+      </ul>
+
+      <Link to="/">Ir para home</Link>
     </PageDefault>
   );
 };
