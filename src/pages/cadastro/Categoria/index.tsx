b930206294cdddabdd70efb1ff2627960ce7b7e0
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-29 17:52:17 -0300

    Listagem categoria

--- src/pages/cadastro/Categoria/index.tsx
+++ src/pages/cadastro/Categoria/index.tsx
@@ -1,21 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import PageDefault from '../../../components/PageDefault';
 
 const Categoria: React.FC = () => {
+  const [novaCategoria, setNovaCategoria] = useState('');
+  const [listaCategorias, setListaCategorias] = useState<string[]>([]);
+
+  function handleInput(event: React.ChangeEvent<HTMLInputElement>) {
+    const newCategoria = event.target.value;
+    setNovaCategoria(newCategoria);
+  }
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    setListaCategorias([...listaCategorias, novaCategoria]);
+    setNovaCategoria('');
+  }
+
   return (
     <PageDefault>
       <h1>Cadastro de Categoria</h1>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="categoria">
           Nome da Categoria:
-          <input id="categoria" type="text" />
+          <input
+            id="categoria"
+            type="text"
+            value={novaCategoria}
+            onChange={handleInput}
+          />
         </label>
 
-        <button type="button">Cadastrar</button>
+        <button type="submit">Cadastrar</button>
       </form>
+
+      <ul>
+        {listaCategorias.map((categoria) => (
+          <li key={categoria}>{categoria}</li>
+        ))}
+      </ul>
 
       <Link to="/">Ir para home</Link>
     </PageDefault>
