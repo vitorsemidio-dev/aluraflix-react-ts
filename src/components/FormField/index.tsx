ee98da8055ed76f841fc369105610d0a6f565e0d
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-30 18:03:55 -0300

    Criação componente FormField e copia estilos

---
+++ src/components/FormField/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { FormFieldWrapper, Label, LabelText, Input } from './styles';
+
+const FormField: React.FC = () => {
+  return (
+    <div>
+      <span>FormField Component</span>
+    </div>
+  );
+};
+
+export default FormField;
