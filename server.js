16d0c94f1efe119c48efc2ce01d4eab8837c9fe0
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-08-01 16:57:22 -0300

    Primeiros dados no novo db

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const jsonServer = require('json-server');
 
 const server = jsonServer.create();
 // const router = jsonServer.router('db.json');
-const router = jsonServer.router('nathflix.json');
+const router = jsonServer.router('emidio.json');
+// const router = jsonServer.router('nathflix.json');
 const middlewares = jsonServer.defaults();
 
 const port = process.env.PORT || 8080;
